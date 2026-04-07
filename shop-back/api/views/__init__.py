f0a9184cea991b0d9d9fb0325da621d710a0c478
Alinur <UltraMonkey@bk.ru>
2026-04-08 03:58:06 +0500

    Bruh

---
+++ shop-back/api/views/__init__.py
@@ -0,0 +1,10 @@
+# Switch between implementations by changing the import source.
+# Currently active: Level 5 (Generics)
+
+from api.views.generics import (
+    ProductListAPIView,
+    ProductDetailAPIView,
+    CategoryListAPIView,
+    CategoryDetailAPIView,
+    CategoryProductsAPIView,
+)
